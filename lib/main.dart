08dc714ad233e106c640c561de72d3006bd03323
rsyd29 <brasyid15@gmail.com>
2022-07-09 13:29:03 +0700

    remove import package developer no need

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:my_counter_cubit/cubits/counter/counter_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
